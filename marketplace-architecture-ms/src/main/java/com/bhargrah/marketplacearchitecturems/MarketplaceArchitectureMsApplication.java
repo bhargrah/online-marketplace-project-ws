3d4a16052463ee7a793b90a7df396f8dd2a4817e
bhargrah <rahulbhargava21@gmail.com>
2023-07-15 12:01:55 +0000

    Adding Architecture Project [C4 Repo]

---
+++ marketplace-architecture-ms/src/main/java/com/bhargrah/marketplacearchitecturems/MarketplaceArchitectureMsApplication.java
@@ -0,0 +1,13 @@
+package com.bhargrah.marketplacearchitecturems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketplaceArchitectureMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketplaceArchitectureMsApplication.class, args);
+	}
+
+}
